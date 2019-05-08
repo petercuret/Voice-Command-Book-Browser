53e5e980b1b243d30ef1f499ad77ab072b5d8a81
Peter Curet <>
2019-05-08 18:35:13 +0200

    Carousel scrolling

--- task-2/src/components/bookCarousel/book-carousel.ts
+++ task-2/src/components/bookCarousel/book-carousel.ts
@@ -15,13 +15,63 @@ class BookCarousel extends HTMLElement {
 
   render() {
     this.innerHTML = `
-    ${this.getElementStyling()}
-    <section></section>
+      ${this.getElementStyling()}
+      <section></section>
     `;
+  }
+
+  isInViewport(element : Element) {
+    const rectangle : DOMRect = <DOMRect>element.getBoundingClientRect();
+    const viewportWidth : number = window.innerWidth;
+    const rightSideX = (rectangle.x + rectangle.width) - 1;
+    return rightSideX < (viewportWidth);
+  }
+
+  scrollToBook(book : Element) {
+    book.scrollIntoView({behavior: "smooth", block: "end", inline: "start"});
+  }
+
+  getBookElements() {
+    return Array.from(this.querySelector('section').children);
+  }
+
+  scrollToFirstBook() {
+    const books = this.getBookElements();
+
+    if(books && books.length > 0) {
+      const firstBook = books[0];
+      this.scrollToBook(firstBook); 
+     }    
+  }
+
+  // TODO: Scroll can probably be implemented a bit nicer, revisit if there's time
+  scroll() {
+    const books = this.getBookElements();
+
+    for(let i = 0; i < books.length; i++) {
+      const book = books[i];
+      if(!this.isInViewport(book)) {
+        this.scrollToBook(book);
+        return;
+      }
+    };
+
+    // The loop wasn't broken, it means we've reached the last book
+    this.scrollToFirstBook();
+  }
+
+  addScrollTimeout() {
+    window.setInterval(() => {
+      // Scroll if the page is visible (tab is active)
+      if (!document.hidden) {
+        this.scroll();
+      }      
+    }, 3000);
   }
 
   connectedCallback() {
     this.render();
+    this.addScrollTimeout();
   }
 }
 
